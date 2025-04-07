0f1c71027a7ad9f344e8c9944e43a62d0b52eefc
Marek Belski <marek.belski@gmail.com>
2025-04-07 14:52:04 -0700

    Fixes checks for getting current jam stage.

--- addons/gwj_countdown/scenes/countdown_ui.gd
+++ addons/gwj_countdown/scenes/countdown_ui.gd
@@ -22,6 +22,12 @@ const JAM_FIRST_YEAR = 2018
 @onready var stage_label = %StageLabel
 @onready var countdown_label = %CountdownLabel
 
+@export_range(1, 3) var precision : int = 2
+
+@export_group("Debug")
+@export var adjust_days : int = 0
+@export var adjust_hours : int = 0
+
 func _get_2nd_friday(day : int, weekday : int) -> int:
 	var weekday_diff := weekday - TARGET_WEEKDAY
 	var target_relative_day := (day - weekday_diff)
@@ -29,33 +35,46 @@ func _get_2nd_friday(day : int, weekday : int) -> int:
 	var target_day = target_first_day + (7 * (TARGET_WEEKDAY_OCCURRENCE - 1))
 	return target_day
 
+func adjust_datetime_dict(datetime_dict : Dictionary) -> Dictionary:
+	var _adjust_days := adjust_days
+	if adjust_hours:
+		datetime_dict["hour"] += adjust_hours
+		if datetime_dict["hour"] >= 24:
+			_adjust_days += 1
+		datetime_dict["hour"] %= 24
+	if adjust_days:
+		datetime_dict["day"] += _adjust_days
+		datetime_dict["weekday"] += _adjust_days
+		datetime_dict["weekday"] += 1
+		datetime_dict["weekday"] %= 7
+		datetime_dict["weekday"] -= 1
+	return datetime_dict
+
+func _update_dict_to_months_jam(datetime_dict : Dictionary) -> Dictionary:
+	var jam_start_day = _get_2nd_friday(datetime_dict["day"], datetime_dict["weekday"])
+	datetime_dict["day"] = jam_start_day
+	datetime_dict["weekday"] = TARGET_WEEKDAY
+	datetime_dict["hour"] = TARGET_HOUR
+	datetime_dict["minute"] = 0
+	datetime_dict["second"] = 0
+	return datetime_dict
+
 func _get_delta_time_until_next_month_jam() -> int:
-	var current_time_unix := int(Time.get_unix_time_from_system())
+	var current_time_dict := Time.get_datetime_dict_from_system(true)
+	current_time_dict = adjust_datetime_dict(current_time_dict)
+	var current_time_unix := int(Time.get_unix_time_from_datetime_dict(current_time_dict))
 	var next_month_unix = current_time_unix + (MIN_DAYS_PER_MONTH * SECONDS_PER_DAY)
 	var next_month_dict := Time.get_datetime_dict_from_unix_time(next_month_unix)
-	var day = next_month_dict["day"]
-	var weekday = next_month_dict["weekday"]
-	var jam_start_day = _get_2nd_friday(day, weekday)
-	next_month_dict["day"] = jam_start_day
-	next_month_dict["weekday"] = TARGET_WEEKDAY
-	next_month_dict["hour"] = TARGET_HOUR
-	next_month_dict["minute"] = 0
-	next_month_dict["second"] = 0
+	next_month_dict = _update_dict_to_months_jam(next_month_dict)
 	var jam_time_unix := Time.get_unix_time_from_datetime_dict(next_month_dict)
 	return jam_time_unix - current_time_unix
 
 func _get_delta_time_until_jam() -> int:
 	var current_time_dict := Time.get_datetime_dict_from_system(true)
-	var day = current_time_dict["day"]
-	var weekday = current_time_dict["weekday"]
-	var jam_start_day = _get_2nd_friday(day, weekday)
-	var jam_time_dict = current_time_dict.duplicate()
-	jam_time_dict["day"] = jam_start_day
-	jam_time_dict["weekday"] = TARGET_WEEKDAY
-	jam_time_dict["hour"] = TARGET_HOUR
-	jam_time_dict["minute"] = 0
-	jam_time_dict["second"] = 0
+	current_time_dict = adjust_datetime_dict(current_time_dict)
 	var current_time_unix := Time.get_unix_time_from_datetime_dict(current_time_dict)
+	var jam_time_dict = current_time_dict.duplicate()
+	jam_time_dict = _update_dict_to_months_jam(jam_time_dict)
 	var jam_time_unix := Time.get_unix_time_from_datetime_dict(jam_time_dict)
 	return jam_time_unix - current_time_unix
 
@@ -67,7 +86,6 @@ func _get_countdown_string(delta_time : int) -> String:
 	countdown_array.append((delta_time % SECONDS_PER_DAY % SECONDS_PER_HOUR) / SECONDS_PER_MINUTE)
 	countdown_array.append(delta_time % SECONDS_PER_DAY % SECONDS_PER_HOUR % SECONDS_PER_MINUTE)
 	var iter := -1
-	var display_length := 2
 	var displayed_count := 0
 	for countdown_value in countdown_array:
 		iter += 1
@@ -87,20 +105,29 @@ func _get_countdown_string(delta_time : int) -> String:
 			countdown_string += "s"
 		countdown_string += " "
 		displayed_count += 1
-		if displayed_count >= display_length:
+		if displayed_count >= precision:
 			break
 	return countdown_string
 
+func _unix_is_after_jam(unix_time : int) -> bool:
+	return unix_time > (JAM_DAYS + VOTING_DAYS) * SECONDS_PER_DAY
+
+func _unix_is_voting_period(unix_time : int) -> bool:
+	return unix_time > JAM_DAYS * SECONDS_PER_DAY and unix_time <= (JAM_DAYS + VOTING_DAYS) * SECONDS_PER_DAY
+
+func _unix_is_jam_period(unix_time : int) -> bool:
+	return unix_time > 0 and unix_time <= JAM_DAYS * SECONDS_PER_DAY
+
 func refresh_text():
 	var delta_time_unix := _get_delta_time_until_jam()
-	var jam_days_delta := delta_time_unix / SECONDS_PER_DAY
-	if -jam_days_delta >= JAM_DAYS + VOTING_DAYS:
+	if _unix_is_after_jam(-delta_time_unix):
 		# Today is passed the current month's jam. Get next months jam.
 		delta_time_unix = _get_delta_time_until_next_month_jam()
-	elif -jam_days_delta >= JAM_DAYS and -jam_days_delta < JAM_DAYS + VOTING_DAYS:
+		stage_label.text = DEFAULT_STAGE_STRING
+	elif _unix_is_voting_period(-delta_time_unix):
 		stage_label.text = VOTING_STAGE_STRING
 		delta_time_unix += (JAM_DAYS + VOTING_DAYS) * SECONDS_PER_DAY
-	elif -jam_days_delta >= 0 and -jam_days_delta < JAM_DAYS:
+	elif _unix_is_jam_period(-delta_time_unix):
 		stage_label.text = JAM_STAGE_STRING
 		delta_time_unix += JAM_DAYS * SECONDS_PER_DAY
 	else:
